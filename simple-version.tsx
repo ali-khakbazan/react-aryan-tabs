0aaa5e246870e1a721f46f1b52d59879c6a5183c
Ali Khakbazan <99426160+ali-khakbazan@users.noreply.github.com>
2022-06-05 20:10:12 +0430

    Update simple-version.tsx

--- simple-version.tsx
+++ simple-version.tsx
@@ -15,7 +15,7 @@ type TabsProps = PropsWithChildren<{
   tabStyles?: (styles: CSSProperties) => CSSProperties;
   tabBorderStyles?: (styles: CSSProperties) => CSSProperties;
   rtl?: boolean;
-  activeCollor?: string;
+  activeBorderCollor?: string;
 }>;
 
 export const Tabs: React.FC<TabsProps> = ({
@@ -23,7 +23,7 @@ export const Tabs: React.FC<TabsProps> = ({
   bodyClassName,
   wrapperStyles,
   tabBorderStyles,
-  activeCollor = "#3498db",
+  activeBorderCollor = "#3498db",
   tabStyles,
   rtl,
   children,
@@ -108,7 +108,7 @@ export const Tabs: React.FC<TabsProps> = ({
       right: borderStyles?.left ? "unset" : "8px",
       left: borderStyles?.left ?? "unset",
       width: borderStyles?.width ?? "40px",
-      backgroundColor: activeCollor,
+      backgroundColor: activeBorderCollor,
       height: "2px",
       bottom: "0px",
       position: "absolute",
@@ -119,7 +119,7 @@ export const Tabs: React.FC<TabsProps> = ({
     }
 
     return defaultStyles;
-  }, [tabBorderStyles, borderStyles?.left, borderStyles?.width, activeCollor]);
+  }, [tabBorderStyles, borderStyles?.left, borderStyles?.width, activeBorderCollor]);
 
   return (
     <div dir={rtl ? "rtl" : "ltr"}>
